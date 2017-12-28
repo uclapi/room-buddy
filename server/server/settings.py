7bd79ee39ec541e0ed896e585d8a5129da5b0a4d
Wilhelm Klopp <wil.klopp@gmail.com>
2017-12-28 23:07:22 +0100

    Make django debug environment conditional

--- server/server/settings.py
+++ server/server/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ['DJANGO_SECRET']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ['DJANGO_DEBUG'] == 'True'
 
 ALLOWED_HOSTS = [os.environ['APP_HOST']]
 
